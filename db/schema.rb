8e8dca6369973cbd3894a95487a97388eac1e7ab
yuichiyasui <yassan.b.a.w@gmail.com>
2021-11-03 23:44:39 +0900

    seedsを作成

---
+++ db/schema.rb
@@ -0,0 +1,28 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20_211_103_124_701) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension 'plpgsql'
+
+  create_table 'members', force: :cascade do |t|
+    t.string 'first_name', null: false
+    t.string 'first_name_hiragana', null: false
+    t.string 'last_name', null: false
+    t.string 'last_name_hiragana', null: false
+    t.string 'handle_name', null: false
+    t.datetime 'birthday', null: false
+    t.text 'comment', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Member.create(
+  [
+    {
+      first_name: '太郎',
+      first_name_hiragana: 'たろう',
+      last_name: 'テスト',
+      last_name_hiragana: 'てすと',
+      handle_name: '桃太郎',
+      birthday: '1998-11-03',
+      comment: 'よろしくお願いします。'
+    }
+  ]
+)
