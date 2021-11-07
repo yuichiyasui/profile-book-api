493b6681b2b925d2036b7273efaa06df19b8c6d6
yuichiyasui <yassan.b.a.w@gmail.com>
2021-11-07 18:53:39 +0900

    member_typeにhobbiesを追加

--- app/graphql/types/hobby_type.rb
+++ app/graphql/types/hobby_type.rb
@@ -5,6 +5,7 @@ module Types
     field :category_id, Integer, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :category, Types::CategoryType, null: true
+
+    field :category, Types::CategoryType, null: false
   end
 end
