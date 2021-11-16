26484ee0e3025c4af3269ad3513d1a32dabf52cd
yuichiyasui <yassan.b.a.w@gmail.com>
2021-11-17 00:52:31 +0900

    メンバー作成のmutationを追加

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
+    field :create_member, mutation: Mutations::CreateMember
     field :create_category, mutation: Mutations::CreateCategory
   end
 end
