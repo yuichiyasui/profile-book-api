98b2ae491487f7765e6e2e208e22a274951fd877
yuichiyasui <yassan.b.a.w@gmail.com>
2021-11-17 01:38:43 +0900

    特技を登録するmutationを作成

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
+    field :create_speciality, mutation: Mutations::CreateSpeciality
     field :create_hobby, mutation: Mutations::CreateHobby
     field :create_member, mutation: Mutations::CreateMember
     field :create_category, mutation: Mutations::CreateCategory
