c5e963eb4295af5d1f7f33d038bd690636292eae
yuichiyasui <yassan.b.a.w@gmail.com>
2021-12-07 23:45:04 +0900

    Userモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_05_083009) do
+ActiveRecord::Schema.define(version: 2021_12_07_143828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,6 +86,15 @@ ActiveRecord::Schema.define(version: 2021_12_05_083009) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_temporary_users_on_email", unique: true
     t.index ["uuid"], name: "index_temporary_users_on_uuid", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.integer "role", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "hobbies", "categories"
