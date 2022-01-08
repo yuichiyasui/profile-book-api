dbff7e1d37e4e28bf3b226d5072cf7650f0f38fa
yuichiyasui <yassan.b.a.w@gmail.com>
2022-01-08 16:39:39 +0900

    仮ユーザーから本ユーザーへ移行できない不具合を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,8 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /[\w\-._]+@[\w\-._]+\.[A-Za-z]+/
-  VALID_PASSWORD_REGEX = /((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])|(?=.*[a-z])(?=.*[A-Z])(?=.*[!@_#¥=?;:])|(?=.*[A-Z])(?=.*[0-9])(?=.*[!@_#¥=?;:])|(?=.*[a-z])(?=.*[0-9])(?=.*[!@_#¥=?;:]))/
 
   has_secure_password
   validates :email,
             uniqueness: true,
             format: { with: VALID_EMAIL_REGEX }
-  validates :password,
-            presence: true,
-            length: { minimum: 8 },
-            format: { with: VALID_PASSWORD_REGEX }
 end
