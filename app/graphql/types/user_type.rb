9bef76d97b17051e7c12461dc0d2f6e28a3eb338
yuichiyasui <yassan.b.a.w@gmail.com>
2022-01-09 23:58:58 +0900

    ユーザータイプを修正

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,9 +1,18 @@
 module Types
   class UserType < Types::BaseObject
+    description 'ユーザー情報'
+
     field :id, ID, null: false
     field :email, String, null: false
-    field :role, Integer, null: false
-    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :authentication_token, String, null: false
+
+    def authentication_token
+      if object.gql_id != context[:current_user]&.gql_id
+        raise GraphQL::UnauthorizedFieldError,
+              'Unable to access authentication_token'
+      end
+
+      object.authentication_token
+    end
   end
 end
