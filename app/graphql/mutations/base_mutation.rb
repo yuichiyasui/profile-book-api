5492d06bf33f3454484ce599d70411b52cf0017c
yuichiyasui <yassan.b.a.w@gmail.com>
2022-01-09 23:32:14 +0900

    Mutationでユーザーが認証済みか確認するメソッドを追加

--- app/graphql/mutations/base_mutation.rb
+++ app/graphql/mutations/base_mutation.rb
@@ -4,5 +4,13 @@ module Mutations
     field_class Types::BaseField
     input_object_class Types::BaseInputObject
     object_class Types::BaseObject
+
+    protected
+
+    def authorize_user
+      return true if context[:current_user].present?
+
+      raise GraphQL::ExecutionError, 'User not signed in'
+    end
   end
 end
