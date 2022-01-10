9a4d4bf516078d7c98c11c56ec6f09aac04da5c0
yuichiyasui <yassan.b.a.w@gmail.com>
2022-01-10 11:34:55 +0900

    既存のユーザーモデルを削除

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ApplicationRecord
-  VALID_EMAIL_REGEX = /[\w\-._]+@[\w\-._]+\.[A-Za-z]+/
-
-  has_secure_password
-  validates :email,
-            uniqueness: true,
-            format: { with: VALID_EMAIL_REGEX }
-end
