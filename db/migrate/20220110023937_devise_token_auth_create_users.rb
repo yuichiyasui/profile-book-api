600710923962262d42804d861d22e15f6b6dca09
yuichiyasui <yassan.b.a.w@gmail.com>
2022-01-11 23:31:15 +0900

    usersテーブルから不要なカラムを削除

--- db/migrate/20220110023937_devise_token_auth_create_users.rb
+++ db/migrate/20220110023937_devise_token_auth_create_users.rb
@@ -1,18 +1,17 @@
 class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[6.0]
   def change
-    
     create_table(:users) do |t|
       ## Required
-      t.string :provider, :null => false, :default => "email"
-      t.string :uid, :null => false, :default => ""
+      t.string :provider, null: false, default: 'email'
+      t.string :uid, null: false, default: ''
 
       ## Database authenticatable
-      t.string :encrypted_password, :null => false, :default => ""
+      t.string :encrypted_password, null: false, default: ''
 
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
-      t.boolean  :allow_password_change, :default => false
+      t.boolean  :allow_password_change, default: false
 
       ## Rememberable
       t.datetime :remember_created_at
@@ -29,9 +28,6 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[6.0]
       # t.datetime :locked_at
 
       ## User Info
-      t.string :name
-      t.string :nickname
-      t.string :image
       t.string :email
 
       ## Tokens
@@ -40,8 +36,8 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[6.0]
       t.timestamps
     end
 
-    add_index :users, :email,                unique: true
-    add_index :users, [:uid, :provider],     unique: true
+    add_index :users, :email, unique: true
+    add_index :users, %i[uid provider], unique: true
     add_index :users, :reset_password_token, unique: true
     add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
