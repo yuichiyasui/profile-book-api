622168dba88460d0cfef4634b3ab4005cbd99410
yuichiyasui <yassan.b.a.w@gmail.com>
2022-01-14 00:38:15 +0900

    graphql_deviseのマウントをschemaで行うように変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,5 @@
 include GraphqlDevise::MailerHelper
 Rails.application.routes.draw do
-  mount_graphql_devise_for(
-    User,
-    at: '/graphql_auth',
-    operations: {
-      # login: Mutations::Login,
-      register: Mutations::SignUp,
-      confirm_registration_with_token: Mutations::ConfirmRegistrationWithToken
-    }
-  )
   post '/graphql', to: 'graphql#execute'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
